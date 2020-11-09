0951ef62dd73b1e39abcf6780c43e491b7e2d67f
thomasmalloch <thomasmalloch@users.noreply.github.com>
2020-11-10 01:30:13 +0900

    static files fix
    favicon fix

--- novagetan/settings/base.py
+++ novagetan/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 env = environ.Env(
     DEBUG=(bool, False)
